605bcbb5b5931c983292fd3cccee55877d8c8a8d
zhang009 <42031101+zhang009@users.noreply.github.com>
2019-10-11 17:31:31 +0800

    version1

--- src/com/test/controller/UserController.java
+++ src/com/test/controller/UserController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.test.service.TeacherService;
-//1234
-//5678
-//abcd
-//efgh
+//12
+//56
+//ab
+//ef
 @Controller
 public class UserController {
 	// ≤‚ ‘∫œ≤¢
